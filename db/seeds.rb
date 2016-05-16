98995919d293cfcb2526f0671769dcb87b416a70
ac-adekunle <lola.adekunle@gmail.com>
2016-05-15 22:20:49 -0500

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,16 @@
 # 51 pages on Viki
-(46..51).each do |num|
-  puts "scraping page #{num}"
-  ScrapeVikiForDramaUrls.new("https://www.viki.com/explore?page=#{num}&sort=latest&type=series").urls.each do |url|
-    puts "now scraping #{url}"
-    ScrapeVikiDramaContent.new(url)
-    sleep 5
-  end
+# (1..51).each do |num|
+#   puts "scraping page #{num}"
+#   ScrapeVikiForDramaUrls.new("https://www.viki.com/explore?page=#{num}&sort=latest&type=series").urls.each do |url|
+#     puts "now scraping #{url}"
+#     ScrapeVikiDramaContent.new(url)
+#     sleep 5
+#   end
+# end
+
+Drama.all.each do |drama|
+  ScrapeVikiDramaContent.new(drama.viki_url)
+  sleep 5
 end
 
 # (4741..).each do |num|
