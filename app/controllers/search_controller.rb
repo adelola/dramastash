ccea467fb5aeb19c3c505b6369993ba35fa32e65
ac-adekunle <lola.adekunle@gmail.com>
2016-05-19 02:01:33 -0500

    Debug searchkick

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,9 @@ class SearchController < ApplicationController
   respond_to :json, :html
 
   def search
-    dramas = Drama.search(params[:q])
+    dramas = Drama.search(params[:q]), fields: [:name, :also_known_as]
     users = User.where(username: params[:q])
-    casts = Cast.search(params[:q])
+    casts = Cast.search(params[:q]), fields: [:name, :non_english_name]
     @results = {dramas: dramas, users: users, casts: casts}
     if @results 
       respond_with(@results)
