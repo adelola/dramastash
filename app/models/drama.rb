7f4c00913ac58a4d904035bf7d877af871afaa30
ac-adekunle <lola.adekunle@gmail.com>
2016-05-20 01:15:34 -0500

    Change from asc to desc

--- app/models/drama.rb
+++ app/models/drama.rb
@@ -31,7 +31,7 @@ class Drama < ActiveRecord::Base
   end
 
   def self.fetch
-    Drama.where.not(poster_file_name: nil).order("broadcast_period ASC")
+    Drama.where.not(poster_file_name: nil).order("broadcast_period DESC")
   end
 
   def add_image_url
