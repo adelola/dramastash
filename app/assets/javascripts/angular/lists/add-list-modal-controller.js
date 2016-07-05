03706198cbaac0f91495b270e36de92c2cb7a85f
ac-adekunle <lola.adekunle@gmail.com>
2016-07-05 15:41:37 -0500

    Debug modals

--- app/assets/javascripts/angular/lists/add-list-modal-controller.js
+++ app/assets/javascripts/angular/lists/add-list-modal-controller.js
@@ -1,16 +1,17 @@
 angular
   .module('secondLead')
-  .controller('AddListModalCtrl', ['$uibModalInstance', function ($scope, $uibModalInstance){
+  .controller('AddListModalCtrl', ['$scope', '$uibModalInstance', function ($scope, $uibModalInstance){
 
     $scope.name = null;
     $scope.description =null;
 
-
     $scope.close = function () {
+      console.log('closing');
       $uibModalInstance.close({name:$scope.name , description: $scope.description});
     };
 
     $scope.cancel = function () {
+      console.log('canceling');
       $uibModalInstance.dismiss('cancel');
     };
   }]);
